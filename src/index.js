414c1af039ab726f03a1832b76723b78b110e3bb
Kevin Diaz <kevindi0695@gmail.com>
2021-02-24 12:52:44 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hola');
+import '@babel/polyfill';
+import { async } from 'regenerator-runtime';
+import app from './server';
+
+import {connect} from './database' ;
+
+ async function main(){
+
+        await app.listen(app.get('port')); 
+        console.log('Server escuchando en puesrto ' + app.get('port'));
+        
+        await connect(); 
+};
+
+main();
