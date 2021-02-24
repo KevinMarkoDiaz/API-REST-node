379d2e566fef52b9f30b5f0d01f2d2f1d082d356
Kevin Diaz <kevindi0695@gmail.com>
2021-02-24 18:47:46 -0300

    confirmo

--- src/routes/idea.routes.js
+++ src/routes/idea.routes.js
@@ -3,7 +3,7 @@ const router = Router();
 
 //Database connection
 
-
+import { ObjectID } from "mongodb";
 import { connect } from "../database";
 
 router.get('/', async (req,res) => {
@@ -27,12 +27,43 @@ const ideasingular = {
 
 const result = await db.collection('ideas').insertOne(ideasingular);
 
-console.log(result);
 
-res.json('creaste un dato');
+res.json(result.ops[0]);
 });
 
 
+
+router.get('/:id',async(req,res)=>{
+const {id} = req.params;
+const db = await connect();
+const result = await db.collection('ideas').findOne( {_id: ObjectID(id)});
+
+ res.json(result);
+});
+
+
+router.delete('/:id', async(req,res)=>{
+    const {id} = req.params;
+    const db = await connect();
+    const result = await db.collection('ideas').findOneAndDelete({_id: ObjectID(id)});
+    res.json(`message:La idea fue eliminada correctamente idea: ${id} ` + result
+    
+    );
+});
+
+
+router.put('/:id', async (req,res)=>{
+const {id} = req.params;
+const updateIdea = {
+    tittle : req.body.tittle,
+    description : req.body.description
+}
+
+const db = await connect();
+const result = await db.collection('ideas').updateOne({_id: ObjectID(id) }
+                , {$set: updateIdea});
+res.json("tarea realizada");
+});
 
 
 export default router;
